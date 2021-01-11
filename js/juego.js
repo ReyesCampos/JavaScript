48153f1d31a4fe83ef20d01a12cd85c0fa93ae5e
ReyesCampos <71782217+ReyesCampos@users.noreply.github.com>
2021-01-11 09:18:46 -0700

    JSON y Arrays en JS

--- js/juego.js
+++ js/juego.js
@@ -1,9 +1,25 @@
 //Se asegura de que se ha cargado toda la pagina
+var indiceRandom=0;
+var op=[];
 $(document).ready(function(){
 
-    $("#login").animate({
+    /*$("#login").animate({
         'top':"10%"
-    },300);
+    },300);*/
+    $.getJSON('https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json', function(data){
+        //console.log(data.elements[0].name);
+        var cuantos = data.elements.length;
+        indiceRandom = Math.floor(Math.random() * cuantos);
+        $("#Elemento").find('h2').text(data.elements[indiceRandom].name);
+        for(var i = 0; i<3; i++){
+            var n = Math.floor(Math.random() * cuantos);
+            op.push(n);
+            $("#Opciones").find('ul').append('<li><h2>' + data.elements[n].symbol + '</h2></li>');
+        }
+        $("#Opciones").find('ul').append('<li><h2>' + data.elements[indiceRandom].symbol + '</h2></li>');
+        op.push(indiceRandom);
+        console.log(op);
+    });
     $("txtNombre").on('keyup',function(){
         $("#txtNombre").css('border', '1px solid white');
         $(".alerta").fadeOut(400);
